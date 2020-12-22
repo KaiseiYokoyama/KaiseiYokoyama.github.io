55642c56a9a50cc57e7cb9033a6ec93a69074da7
KYokoyama <8509057+KaiseiYokoyama@users.noreply.github.com>
2020-12-22 16:40:34 +0900

    Citationのコピー機能を実装

--- src/assets/js/base.js
+++ src/assets/js/base.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
     M.AutoInit();
+
+    // citationをクリップボードにコピーするボタン
+    document.querySelectorAll('#papers .citation-item')
+        .forEach((itemElem) => {
+            const citation = itemElem.innerText;
+
+            const icon = document.createElement('i');
+            icon.classList.add('material-icons');
+            icon.innerText = 'content_copy';
+
+            const copyButton = document.createElement('a');
+            copyButton.classList.add('btn-flat', 'copy-citation');
+            copyButton.onclick = () => {
+                navigator.clipboard.writeText(citation)
+                    .then(M.toast({html: 'Citation Copied!'}))
+            }
+
+            copyButton.appendChild(icon);
+            itemElem.appendChild(copyButton);
+        })
 });
